5e2ba62c2b05deaaf07d39c8f7d0853f48992150
Nishat Ayub <nishatayub702@gmail.com>
2025-03-16 01:28:19 +0530

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,19 @@
 // write button card here
+import React from 'react'
+
+const button = () => {
+  return (
+    <div style={{
+        border: "1px solid #ddd",
+        borderRadius: "10px",
+        padding: "20px",
+        textAlign: "center",
+        width: "250px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+    }}>
+      View Product
+    </div>
+  )
+}
+
+export default button
